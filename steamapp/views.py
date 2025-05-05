6414c87c773d7eb465d67144c089e993b72a34e4
Alex David Frías <adavidfruiz@gmail.com>
2025-05-05 00:01:40 -0500

    feat: add buscar_juegos_view function to handle game search requests

---
+++ steamapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .steam_service import buscar_juegos
+
+def buscar_juegos_view(request):
+    query = request.GET.get('query', '')
+    juegos = []
+
+    if query:
+        juegos = buscar_juegos(query)  # Usa tu servicio de API directamente
+
+    return render(request, 'resultados_busqueda.html', {
+        'query': query,
+        'juegos': juegos,
+    })
+
