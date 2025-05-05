43b97778d8e77153a5454a4a00e218e18b784716
Alex David Frías <adavidfruiz@gmail.com>
2025-05-05 00:00:05 -0500

    feat: reorganize URL patterns for juego views

--- juegos/urls.py
+++ juegos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("juego/<int:appid>/", views.detalle_juego, name="detalle_juego"),
-    
-    ]
+    path('', views.inicio, name='inicio'),  # Nueva vista de redirección
+    path('buscar/', views.buscar_juego, name='detalle_juego_por_id'),
+    path('<int:appid>/', views.detalle_juego, name='detalle_juego'),
+]
