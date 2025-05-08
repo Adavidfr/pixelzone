0497004e611b53259260828fb85acaa9e6c51ba0
Alex David Frías <adavidfruiz@gmail.com>
2025-05-08 00:37:07 -0500

    chore: clean up commented paths in juegos URL configuration

--- juegos/urls.py
+++ juegos/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),  # Nueva vista de redirección
-    path('buscar/', views.buscar_juego, name='detalle_juego_por_id'),
+    # path('', views.inicio, name='inicio'),
+    path('juegos/', views.lista_juegos, name='lista_juegos'),
+    path('agregar/', views.agregar_juego, name='agregar_juego'),
+    # path('buscar/', views.buscar_juego, name='detalle_juego_por_id'),
     path('<int:appid>/', views.detalle_juego, name='detalle_juego'),
 ]
