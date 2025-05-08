e4a54a3ee801f1ba240a6d6382ed26e2a7c5412d
Alex David Frías <adavidfruiz@gmail.com>
2025-05-08 00:37:02 -0500

    feat: update Juego model to include multiple image fields and video URL

--- juegos/models.py
+++ juegos/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
 class Juego(models.Model):
-    appid = models.IntegerField(unique=True)
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    imagen = models.URLField()
+    imagen_miniatura = models.URLField(null=True, blank=True)
+    imagen_principal = models.URLField(null=True, blank=True)
+    imagen2 = models.URLField(null=True, blank=True)
+    imagen3 = models.URLField(null=True, blank=True)
+    imagen4 = models.URLField(null=True, blank=True)
+    imagen5 = models.URLField(null=True, blank=True)
+    video = models.URLField(null=True, blank=True)    
     descripcion = models.TextField()
     fecha_lanzamiento = models.DateField()
     desarrollador = models.CharField(max_length=100)
     genero = models.CharField(max_length=50)
     descuento = models.IntegerField()
-    es_gratis = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nombre
