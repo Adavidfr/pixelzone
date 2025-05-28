cbdd1e7c71992f5f1ea6f83322a35b28b7e5556a
Alex David Frías <adavidfruiz@gmail.com>
2025-05-28 00:48:02 -0500

    fix: correct path parameter for detalle_juego and ensure consistency in editing route

--- juegos/urls.py
+++ juegos/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('juegos/', views.lista_juegos, name='lista_juegos'),
     path('agregar/', views.agregar_juego, name='agregar_juego'),
     # path('buscar/', views.buscar_juego, name='detalle_juego_por_id'),
-    path('<int:appid>/', views.detalle_juego, name='detalle_juego'),
+    path('<int:id>/', views.detalle_juego, name='detalle_juego'),
+    path('editar/<int:id>/', views.editar_juego, name='editar_juego'),
 ]
