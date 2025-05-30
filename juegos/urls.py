f64027e299c0723f8636be4f0cdfabf2ccb23482
Alex David Frías <adavidfruiz@gmail.com>
2025-05-29 23:41:51 -0500

    feat: add URL pattern for eliminar_juego to enable game deletion functionality

--- juegos/urls.py
+++ juegos/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('buscar/', views.buscar_juego, name='detalle_juego_por_id'),
     path('<int:id>/', views.detalle_juego, name='detalle_juego'),
     path('editar/<int:id>/', views.editar_juego, name='editar_juego'),
+    path('eliminar/<int:id>/', views.eliminar_juego, name='eliminar_juego'),
 ]
