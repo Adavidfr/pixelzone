254ab4f7a16c8308e9b2be3c3ee35a2bfe099bf4
Alex David Frías <adavidfruiz@gmail.com>
2025-06-09 01:43:47 -0500

    feat: add redirect for steam_buscar URL to improve navigation

--- juegos/urls.py
+++ juegos/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
+
 
 urlpatterns = [
-    # path('', views.inicio, name='inicio'),
     path('juegos/', views.lista_juegos, name='lista_juegos'),
     path('agregar/', views.agregar_juego, name='agregar_juego'),
-    # path('buscar/', views.buscar_juego, name='detalle_juego_por_id'),
     path('<int:id>/', views.detalle_juego, name='detalle_juego'),
     path('editar/<int:id>/', views.editar_juego, name='editar_juego'),
     path('eliminar/<int:id>/', views.eliminar_juego, name='eliminar_juego'),
+    path('steam_buscar/', lambda request: redirect('buscar_juegos')),
 ]
