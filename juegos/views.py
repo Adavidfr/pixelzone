8e7c073ca98e430051134f8eff3dc5d90fc5af36
Alex David Frías <adavidfruiz@gmail.com>
2025-06-09 01:43:53 -0500

    feat: enhance game listing and management views for improved functionality

--- juegos/views.py
+++ juegos/views.py
@@ -1,26 +1,46 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.contrib.auth.decorators import user_passes_test
 from .models import Juego
 from .forms import JuegoForm
+from django.db.models import Q, Case, When, IntegerField
 
 def lista_juegos(request):
-    juegos = Juego.objects.all()
+    consulta = request.GET.get('q', '')
+    if consulta:
+        # Crear una expresión que da prioridad a los juegos cuyo nombre empieza con la consulta
+        juegos = Juego.objects.annotate(
+            prioridad=Case(
+                When(nombre__istartswith=consulta, then=0),
+                default=1,
+                output_field=IntegerField(),
+            )
+        ).filter(nombre__icontains=consulta).order_by('prioridad', 'nombre')
+    else:
+        juegos = Juego.objects.all()
+    
     return render(request, 'lista_juegos.html', {
         'juegos': juegos,
-        'mostrar_boton_agregar': True
+        'mostrar_boton_agregar': request.user.is_superuser,
+        'consulta': consulta,
     })
 
+def solo_admin(user):
+    return user.is_superuser
 
+@user_passes_test(solo_admin, login_url='/usuarios/login/')
 def agregar_juego(request):
     if request.method == 'POST':
         form = JuegoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('lista_juegos')
+    else:
+        form = JuegoForm()
     return render(request, 'agregar_juego.html', {
-        'form': JuegoForm(),
+        'form': form,
     })
-               
+
 def detalle_juego(request, id):
     juego = get_object_or_404(Juego, id=id)
     imagenes_extra = [juego.imagen2, juego.imagen3, juego.imagen4, juego.imagen5]
@@ -29,6 +49,7 @@ def detalle_juego(request, id):
         'imagenes_extra': imagenes_extra,
     })
 
+@user_passes_test(solo_admin, login_url='/usuarios/login/')
 def editar_juego(request, id):
     juego = get_object_or_404(Juego, id=id)
     if request.method == 'POST':
@@ -43,11 +64,10 @@ def editar_juego(request, id):
         'juego': juego,
     })
 
+@user_passes_test(solo_admin, login_url='/usuarios/login/')
 def eliminar_juego(request, id):
     juego = get_object_or_404(Juego, id=id)
-    
     if request.method == 'POST':
         juego.delete()
         return redirect('lista_juegos')
-
     return render(request, 'confirmar_eliminacion.html', {'juego': juego})
