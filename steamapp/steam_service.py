3401ea06df13f59c086d12d9e21e739064f1e33a
Alex David Frías <adavidfruiz@gmail.com>
2025-06-18 23:58:33 -0500

    refactor: update steam_service.py to load API key from environment variable and clean up code

--- steamapp/steam_service.py
+++ steamapp/steam_service.py
@@ -1,7 +1,10 @@
+import os
+from dotenv import load_dotenv
 from steam_web_api import Steam
 
-KEY = "E45C9A29D547412B6E7FD726562081CF"
+load_dotenv()
 
+KEY = os.getenv("STEAM_API_KEY")
 steam = Steam(KEY)
 
 def buscar_juegos(query):
@@ -10,7 +13,6 @@ def buscar_juegos(query):
         juegos = search_response.get('apps', [])
 
         for juego in juegos:
-            # Usa directamente la imagen de la API si está disponible
             juego['img'] = juego.get('img', "/static/img/no_image_available.png")
 
         return juegos
