8b7c92f9a32375552d8b65c497aad86a09e4c4ee
Alex David Frías <adavidfruiz@gmail.com>
2025-07-07 00:56:16 -0500

    feat: add URL pattern for Steam game detail view

--- steamapp/urls.py
+++ steamapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('buscar_juegos/', views.buscar_juegos_view, name='buscar_juegos'),
+    path('juego-steam/<int:appid>/', views.detalle_juego_steam_view, name='detalle_juego_steam'),
 ]
