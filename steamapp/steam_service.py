58b6ae714dd87f29f47cded9ca2c5163fd56b2b7
Alex David Frías <adavidfruiz@gmail.com>
2025-07-07 00:56:09 -0500

    refactor: enhance game search functionality and improve error handling in obtener_detalle_juego

--- steamapp/steam_service.py
+++ steamapp/steam_service.py
@@ -13,10 +13,39 @@ def buscar_juegos(query):
         juegos = search_response.get('apps', [])
 
         for juego in juegos:
+            # Extraer appid como entero
+            juego['appid'] = juego.get('id', [None])[0]
             juego['img'] = juego.get('img', "/static/img/no_image_available.png")
+
+        # Filtrar los juegos que tengan appid
+        juegos = [j for j in juegos if j.get('appid')]
 
         return juegos
 
     except Exception as e:
         print("ERROR EN BUSQUEDA:", e)
         return []
+
+
+    
+# def obtener_detalle_juego(appid):
+#     try:
+#         # Forzar cc=us para que venga el precio
+#         detalle = steam.apps.get_app_details(appid, cc='us')
+#         return detalle.get(str(appid), {}).get('data', {})
+#     except Exception as e:
+#         print("ERROR EN DETALLE:", e)
+#         return {}
+
+def obtener_detalle_juego(appid):
+    try:
+        detalle = steam.apps.get_app_details(appid)
+        print("DETALLE CRUDO:", detalle)
+        app_data = detalle.get(str(appid), {})
+        if not app_data.get('success'):
+            print(f"API dice que no hay éxito para appid {appid}")
+            return {}
+        return app_data.get('data', {})
+    except Exception as e:
+        print("ERROR EN DETALLE:", e)
+        return {}
