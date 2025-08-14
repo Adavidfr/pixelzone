38ed31fb7bbbdf677bb6c4c92da3489722266d2b
Alex David Frías <adavidfruiz@gmail.com>
2025-08-14 01:42:51 -0500

    feat: add new API endpoints for game list and cart management in urls.py

--- steamapp/urls.py
+++ steamapp/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.juegos_populares_view, name='juegos_populares'),
+    path('lista_juegos_api/', views.juegos_populares_view, name='lista_juegos_api'),
     path('buscar_juegos/', views.buscar_juegos_view, name='buscar_juegos'),
+    path('mas-vendidos/', views.mas_vendidos_api_view, name='mas_vendidos_api'),
     path('juego/<int:appid>/', views.detalle_juego_api_view, name='detalle_juego_api'),
+    path('juego/<int:appid>/agregar/', views.agregar_al_carrito_steam_view, name='agregar_al_carrito_steam'),
+    path('limpiar-cache/', views.limpiar_cache_steam, name='limpiar_cache_steam'),
 ]
